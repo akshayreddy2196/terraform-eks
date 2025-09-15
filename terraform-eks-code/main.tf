7f24fcd7e2f18d941953083b278ee81615b345f3
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-15 10:38:34 +0530

    Update main.tf

--- terraform-eks-code/main.tf
+++ terraform-eks-code/main.tf
@@ -69,32 +69,34 @@ resource "aws_security_group" "eks_node_sg" {
   }
 }
 
-# EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.1.2"
 
-  cluster_name    = "akshay-eks-cluster"
-  cluster_version = "1.29"
+  name               = "akshay-eks-cluster"         
+  kubernetes_version = "1.29"                       
 
-  subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = false
+  enable_cluster_creator_admin_permissions = true   
 
   eks_managed_node_groups = {
     default = {
-      desired_size    = 2
-      max_size        = 3
-      min_size        = 1
-      instance_types  = ["t3.medium"]
-      security_groups = [aws_security_group.eks_node_sg.id]
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
+      ami_type       = "AL2023_x86_64_STANDARD"    
+      tags = {
+        Name = "default-node-group"
+      }
     }
   }
 
   tags = {
     Environment = "dev"
+    Terraform   = "true"
   }
 }
 
@@ -116,6 +118,7 @@ resource "aws_ecr_repository" "app_repo" {
     Environment = "dev"
   }
 }
+
 
 
 
